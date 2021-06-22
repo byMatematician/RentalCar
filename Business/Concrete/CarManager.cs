2b18694c588e71e2c85d7c317db8a8e45186bb05
Kutbettin Kizilkaya <kutbettin.kizilkaya@semine.no>
2021-06-22 12:18:15 +0200

    22.06.2021

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -36,7 +36,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Car>> GetAll()
         {
-            if (DateTime.Now.Hour == 5)
+            if (DateTime.Now.Hour == 10)
             {
                 return new ErrorDataResult<List<Car>>(Messages.MaintenanceTime);
             }
